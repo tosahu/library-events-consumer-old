4a01ebe3f1c4f6e931d34447ce24871ca636de45
SANJAY <SANJAY@HP>
2020-05-30 20:30:48 +0530

    First commit to push project into bitbucket repository

---
+++ library-events-consumer/src/test/java/sks/springboot/kafka/libraryeventsconsumer/LibraryConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package sks.springboot.kafka.libraryeventsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
